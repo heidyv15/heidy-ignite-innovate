1c9846af3e8e2ce9711307e44834958ad51e0113
Heidy V.V. <154488085+heidyv15@users.noreply.github.com>
2026-06-18 14:29:21 -0400

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,27 +1,21 @@
 import Navbar from "@/components/portfolio/Navbar";
 import Hero from "@/components/portfolio/Hero";
+import Projects from "@/components/portfolio/Projects";
 import About from "@/components/portfolio/About";
 import Skills from "@/components/portfolio/Skills";
-import Experience from "@/components/portfolio/Experience";
-import Projects from "@/components/portfolio/Projects";
-import Lavner from "@/components/portfolio/Lavner";
-import Education from "@/components/portfolio/Education";
 import Contact from "@/components/portfolio/Contact";
 
 const Index = () => {
-  return (
-    <main className="min-h-screen bg-background overflow-x-hidden">
-      <Navbar />
-      <Hero />
-      <About />
-      <Skills />
-      <Experience />
-      <Projects />
-      <Lavner />
-      <Education />
-      <Contact />
-    </main>
-  );
+    return (
+          <main className="min-h-screen bg-background overflow-x-hidden">
+                <Navbar />
+                <Hero />
+                <Projects />
+                <About />
+                <Skills />
+                <Contact />
+          </main>main>
+        );
 };
 
 export default Index;
